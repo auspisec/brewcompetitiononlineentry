7a66fb7dda936b441853958d4c827bd558c2ea79
Synthia <synthia@saaz.beer>
2026-09-01 10:37:17 +0000

    Sync upstream PR feedback fixes: pouring coverage, data-loss fix, mead eval, chain style
    Apply all upstream review feedback fixes to the seoul-cup integration
    branch:
    
    - Fix silent data loss in process_brewing.inc.php: preserve existing
      brewPouring JSON on cross-locale edits when radio POST fields are absent
    - Extend translate_pouring_* + h() to admin/entries.admin.php,
      eval/dashboard.eval.php, eval/scoresheet.eval.php, and
      sections/brewer_entries.sec.php
    - Add translate_mead_* + h() to eval/scoresheet.eval.php (brewMead1/2/3)
    - Split method chain in get_default_language() for style consistency

--- eval/scoresheet.eval.php
+++ eval/scoresheet.eval.php
@@ -415,21 +415,21 @@ if ($entry_found) {
   if (!empty($row_entry_info['brewMead1'])) {
     $entry_info_html .= "<div class=\"row bcoem-admin-element\">";
     $entry_info_html .= "<div class=\"col col-lg-3 col-md-4 col-sm-4 col-xs-12\"><strong>".$label_carbonation."</strong></div>";
-    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".$row_entry_info['brewMead1']."</div>";
+    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".h(translate_mead_req_value($row_entry_info['brewMead1']))."</div>";
     $entry_info_html .= "</div>";
   }
 
   if (!empty($row_entry_info['brewMead3'])) {
     $entry_info_html .= "<div class=\"row bcoem-admin-element\">";
     $entry_info_html .= "<div class=\"col col-lg-3 col-md-4 col-sm-4 col-xs-12\"><strong>".$label_strength."</strong></div>";
-    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".$row_entry_info['brewMead3']."</div>";
+    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".h(translate_mead_strength_value($row_entry_info['brewMead3']))."</div>";
     $entry_info_html .= "</div>";
   }
 
   if (!empty($row_entry_info['brewMead2'])) {
     $entry_info_html .= "<div class=\"row bcoem-admin-element\">";
     $entry_info_html .= "<div class=\"col col-lg-3 col-md-4 col-sm-4 col-xs-12\"><strong>".$label_sweetness."</strong></div>";
-    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".$row_entry_info['brewMead2']."</div>";
+    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".h(translate_mead_req_value($row_entry_info['brewMead2']))."</div>";
     $entry_info_html .= "</div>";
   }
 
@@ -494,7 +494,7 @@ if ($entry_found) {
 
     $entry_info_html .= "<div class=\"row bcoem-admin-element\">";
     $entry_info_html .= "<div class=\"col col-lg-3 col-md-4 col-sm-4 col-xs-12\"><strong>".$label_pouring."</strong></div>";
-    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".$pouring_arr['pouring']."</div>";
+    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".h(translate_pouring_value($pouring_arr['pouring']))."</div>";
     $entry_info_html .= "</div>";
 
     if ((isset($pouring_arr['pouring_notes'])) && (!empty($pouring_arr['pouring_notes'])))  {
@@ -506,7 +506,7 @@ if ($entry_found) {
 
     $entry_info_html .= "<div class=\"row bcoem-admin-element\">";
     $entry_info_html .= "<div class=\"col col-lg-3 col-md-4 col-sm-4 col-xs-12\"><strong>".$label_rouse_yeast."</strong></div>";
-    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".$pouring_arr['pouring_rouse']."</div>";
+    $entry_info_html .= "<div class=\"col col-lg-9 col-md-8 col-sm-8 col-xs-12\">".h(translate_pouring_rouse_value($pouring_arr['pouring_rouse']))."</div>";
     $entry_info_html .= "</div>";
 
   }
